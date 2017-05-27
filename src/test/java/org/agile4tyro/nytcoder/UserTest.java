166a13e2b76353691d617d4e57c4ebf33c62053d
Aman Goel <aman.goel@appdirect.com>
2017-05-27 15:31:53 +0530

    Added transaction related files

--- src/test/java/org/agile4tyro/nytcoder/UserTest.java
+++ src/test/java/org/agile4tyro/nytcoder/UserTest.java
@@ -2,27 +2,26 @@ package org.agile4tyro.nytcoder;
 
 import org.agile4tyro.nytcoder.db.User;
 import org.agile4tyro.nytcoder.template.HibernateTemplate;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 
 import junit.framework.TestCase;
 
-;
-
-/**
- * Created by aman on 14/5/17.
- */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UserTest extends TestCase {
 	private final static SessionFactory sessionFactory = HibernateConfig.getConfiguration();
 
-	public void testConnection() {
+	public void test1_Connection() {
 		HibernateTemplate.runQuery(sessionFactory, (Session session) -> {
 			User user = (User) session.createQuery("FROM User").uniqueResult();
 			assertNull(user);
 		});
 	}
 
-	public void testUserInsert() {
+	public void test2_UserCreateDelete() {
 		final User user = User.builder()
 			.username("aman")
 			.password("Goel")
@@ -30,7 +29,6 @@ public class UserTest extends TestCase {
 		HibernateTemplate.runQuery(sessionFactory, (Session session) -> {
 			session.save(user);
 			User dbUser = session.get(User.class, 1l);
-			System.out.println(dbUser);
 			assertNotNull(dbUser);
 			assertEquals(dbUser.getId(), 1l);
 			assertEquals(user.getUsername(), dbUser.getUsername());
@@ -38,6 +36,49 @@ public class UserTest extends TestCase {
 			assertEquals(user.getCreatedOn(), dbUser.getCreatedOn());
 			assertEquals(user.getDeleted(), dbUser.getDeleted());
 		});
+		HibernateTemplate.runQuery(sessionFactory, (Session session) -> {
+			User dbUser = session.get(User.class, 1l);
+			assertNotNull(dbUser);
+			session.delete(dbUser);
+			dbUser = session.get(User.class, 1l);
+			assertNull(dbUser);
+		});
+	}
+
+	public void test3_Transaction() {
+		final User user = User.builder()
+			.username("Hitesh")
+			.password("Yadav")
+			.build();
+
+		HibernateTemplate.runQuery(sessionFactory, (Session session) -> {
+			session.save(user);
+			User dbUser = (User) session.createQuery("FROM User").uniqueResult();
+			assertNotNull(dbUser);
+			assertEquals(user.getUsername(), dbUser.getUsername());
+
+			// Open new session
+			HibernateTemplate.runQuery(sessionFactory, (Session newSession) -> {
+				User testUser = (User) newSession.createQuery("FROM User").uniqueResult();
+				assertNull(testUser);
+			});
+		});
+		HibernateTemplate.runQuery(sessionFactory, (Session session) -> {
+			User dbUser = (User) session.createQuery("FROM User").uniqueResult();
+			assertNotNull(dbUser);
+			session.delete(dbUser);
+			dbUser = (User) session.createQuery("FROM User").uniqueResult();
+			assertNull(dbUser);
+		});
+	}
+
+	public void test4_CurrentSession() {
+		try {
+			Session session = sessionFactory.getCurrentSession();
+			fail("Should have thrown Hibernate exception");
+		} catch (Exception e) {
+			assertTrue(e instanceof HibernateException);
+		}
 	}
 
 }
